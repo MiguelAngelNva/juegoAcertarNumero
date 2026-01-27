841d3d84687ad9d3bba4ac2abeebb6de174b0b0f
Miguel <miguelangelnava_4@hotmail.com>
2026-01-26 19:16:00 -0500

    Se agrengan niveles de partida

--- GameController.java
+++ GameController.java
@@ -7,29 +7,48 @@ public class GameController {
 
         while (true) {
 
+            int level = view.readLevel();
             int min;
             int max;
 
-            // Pedir rango y validar min < max
-            while (true) {
-                min = view.readInt("Ingrese el límite inferior (min):");
-                max = view.readInt("Ingrese el límite superior (max):");
+            /*
+             * while (true) {
+             * min = view.readInt("Ingrese el límite inferior (min):");
+             * max = view.readInt("Ingrese el límite superior (max):");
+             * 
+             * if (min < max) {
+             * break;
+             * }
+             * view.showMessage("El mínimo debe ser menor que el máximo. Intente de
+             * nuevo.");
+             * }
+             */
 
-                if (min < max) {
-                    break;
-                }
-                view.showMessage("El mínimo debe ser menor que el máximo. Intente de nuevo.");
+            if (level == 1) {
+                min = 1;
+                max = 10;
+            } else if (level == 2) {
+                min = 1;
+                max = 100;
+            } else {
+                min = 1;
+                max = 1000;
             }
 
-            // Crear juego con rango válido
-            Game game = new Game(min, max);
-
+            view.showMessage("Nivel seleccionado: " + view.levelName(level));
             view.showMessage("Rango configurado: " + min + " a " + max);
+
+            Game game = new Game(min, max);
             view.showMessage("Adivina el número...");
 
-            // Loop de juego
+            // loop del juego
             while (true) {
                 int userNumber = view.readInt("Ingrese su número:");
+
+                if (userNumber < min || userNumber > max) {
+                    view.showMessage("Debe estar dentro del rango " + min + " a " + max + ".");
+                    continue;
+                }
 
                 String result = game.checkNumber(userNumber);
                 view.showMessage(result);
@@ -39,7 +58,6 @@ public class GameController {
                 }
             }
 
-            // Reinicio completo
             if (!view.askPlayAgain()) {
                 view.showMessage("Gracias por jugar.");
                 break;
